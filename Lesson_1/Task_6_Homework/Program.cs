2f78123ea2f937dca1c9f3312bca1d2e178ac66f
Muxammadali <muxammadali.zhumaev@mail.ru>
2024-02-07 22:49:54 +0500

    add Task_6 - Homework

--- Lesson_1/Task_6_Homework/Program.cs
+++ Lesson_1/Task_6_Homework/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+int num = int.Parse(Console.ReadLine());
+
+if (num % 2 == 1)
+{
+    Console.WriteLine("-> нет");
+}
+else 
+{
+    Console.WriteLine("-> да");
+}
