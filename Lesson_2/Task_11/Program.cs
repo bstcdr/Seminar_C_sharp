dca81c9903f2acbae8889cebd89274857ea7cd0a
Muxammadali <muxammadali.zhumaev@mail.ru>
2024-02-13 07:11:36 +0500

    add Task_11

---
+++ Lesson_2/Task_11/Program.cs
@@ -0,0 +1,14 @@
+﻿// 11.Напишите программу, которая выводит случайное 
+// трёхзначное число и удаляет вторую цифру этого числа.
+// 456 -> 46 782 -> 72 918 -> 98
+
+Random random = new Random();
+
+int randomNum = random.Next(100, 1000);
+
+int firstDigit = randomNum / 100;
+int lastDigit = randomNum % 10;
+
+int result = (firstDigit * 10) + lastDigit;
+Console.WriteLine(randomNum);
+Console.WriteLine(result);
---
+++ Lesson_2/Task_9/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая выводит случайное число 
+// из отрезка [10, 99] и показывает наибольшую цифру числа.
+// 78 -> 8   12 ->2   85-> 8
+
+Random random = new Random();
+
+int randomNum = random.Next(10, 100);
+
+int lastDigit = randomNum % 10;
+int firstDigit = randomNum / 10;
+
+if(lastDigit > firstDigit)
+{
+    Console.WriteLine(lastDigit);
+}
+else
+{
+    Console.WriteLine(firstDigit);
+}
+
+Console.WriteLine(randomNum);
+Console.WriteLine(firstDigit);
+Console.WriteLine(lastDigit);
