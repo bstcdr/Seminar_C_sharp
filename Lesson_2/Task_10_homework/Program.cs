2c55e8b64d611e0168025610793dc55dbefc366b
Muxammadali <muxammadali.zhumaev@mail.ru>
2024-02-18 15:40:17 +0500

    Till the Task_10 - Homework

---
+++ Lesson_2/Task_10_homework/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+Console.Write("Введите число: ");
+
+bool isParsed = false;
+int num = 0;
+while (isParsed == false)
+{
+    isParsed = int.TryParse(Console.ReadLine(), out num);
+    if(isParsed == false)
+    {
+        Console.WriteLine("Пиши числа!");
+    }
+}
+
+if(num > 99 && num < 1000)
+{
+    int secondDigit = (num / 10) % 10;
+    Console.WriteLine($"Вторая цифра -> {secondDigit}");
+}
+else
+{
+    Console.WriteLine("Введите трехзначное число!!!");
+}
